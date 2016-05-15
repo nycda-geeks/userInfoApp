66f865c45edb446a59ffb4a4f49b718f0f0a11ea
Sarith Breedijk <sarith@live.nl>
2016-05-15 13:32:22 +0200

    "Created userInfoApp"
    User Information App - Web Server
    
    Create a Node.js application that is the beginning of a user management
    system. Your users are all saved in a "users.json" file, and you can
    currently do the following:
    - search for users
    - add new new users to your users file.
    - get your starter file here: users.jsonIn een nieuw venster bekijken
    
    Part 0
    Create one route:
    - route 1: renders a page that displays all your users.
    
    Part 1
    Create two more routes:
    - route 2: renders a page that displays a form which is your search bar.
    - route 3: takes in the post request from your form, then displays
    matching users on a new page. Users should be matched based on whether
    either their first or last name contains the input string.
    
    Part 2
    Create two more routes:
    - route 4: renders a page with three forms on it (first name, last
    name, and email) that allows you to add new users to the users.json
    file.
    - route 5: takes in the post request from the 'create user' form, then
    adds the user to the users.json file. Once that is complete, redirects
    to the route that displays all your users (from part 0).

---
+++ app.js
@@ -0,0 +1,91 @@
+// the modules needed for this app
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var bodyParser = require('body-parser')
+
+
+
+
+// renders a page that displays all your users.
+app.set('views', 'src/views'); // views are templates
+app.set('view engine', 'jade');
+
+app.get('/', function(request, response) {
+  fs.readFile('./users.json', function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+
+    var parsedData = JSON.parse(data);
+    console.log(parsedData);
+    response.render("index", {
+      users: parsedData
+    });
+  });
+});
+
+//Create two more routes:
+//- route 2: renders a page that displays a form which is your search bar.
+//- route 3: takes in the post request from your form, then displays matching 
+
+app.use( bodyParser.json() );      
+app.use( bodyParser.urlencoded({    
+  extended: true
+})); 
+app.get('/search', function(request, response){
+	response.render('search')
+});
+
+app.post('/search', function( request, response){
+	fs.readFile('./users.json', function(err, data) {
+    var match = [];
+
+    if (err) {
+      console.log(err);
+    }
+
+    var parsedData = JSON.parse(data);
+
+    console.log('parseddata')
+    console.log(parsedData)
+    
+    var input = request.body.userinput
+    for (var i = 0; i < parsedData.length; i++) {
+    	if (input === parsedData[i].firstname){
+    	match.push(parsedData[i]);
+    	}
+      else if (input === parsedData[i].lastname){
+      match.push(parsedData[i]);
+      }
+    }
+
+    // match = JSON.stringify(match);
+   	console.log(match[0].firstname)
+    console.log(input)
+	 response.render('results', {match: match})
+  });
+});
+
+
+
+app.get('/submit-user', function (request, response) {
+  response.render("submit", {
+  });
+});
+
+
+app.post('/submit-user', function (request, response) {
+  var newuser = request.body
+  var userList = fs.readFileSync('./users.json')
+  var users = JSON.parse (userList)
+    users.push (newuser)
+  var userJSON = JSON.stringify (users)
+    fs.writeFileSync ('./users.json', userJSON)
+      response.redirect ('/')
+ });
+	
+
+var server = app.listen(3000, function() {
+  console.log('Example app listening on port: ' + server.address().port);
+});
