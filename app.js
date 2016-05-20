a641180dcfcaa847edbe38ac5cac661742cae796
Sarith Breedijk <sarith@live.nl>
2016-05-20 17:00:38 +0200

    "added auto shows users"

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ var bodyParser = require('body-parser')
 app.set('views', 'src/views'); // views are templates
 app.set('view engine', 'jade');
 app.use(express.static('src'));
-
+app.use(express.static('../js'));
 
 app.get('/', function(request, response) {
-  fs.readFile('./users.json', function(err, data) {
-    if (err) {
-      console.log(err);
-    }
+	fs.readFile('./users.json', function(err, data) {
+		if (err) {
+			console.log(err);
+		}
 
-    var parsedData = JSON.parse(data);
-    console.log(parsedData);
-    response.render("index", {
-      users: parsedData
-    });
-  });
+		var parsedData = JSON.parse(data);
+		// console.log(parsedData);
+		response.render("index", {
+			users: parsedData
+		});
+	});
 });
 
 //Create two more routes:
@@ -33,65 +33,110 @@ app.get('/', function(request, response) {
 
 app.use( bodyParser.json() );      
 app.use( bodyParser.urlencoded({    
-  extended: true
+	extended: true
 })); 
 app.get('/search', function(request, response){
-	response.render('search')
+	response.render('search') // als ik naar /search ga laadt dan de pagina
+});
+
+app.post('/searchbar', function(request, response){
+ fs.readFile('./users.json', function(err, data) {
+ 	if (err) {
+			console.log(err);
+		}
+		var parsedData = JSON.parse(data);
+		var input = request.body.input;
+		var match = [];
+		var niets = 0;
+		var count = 0;
+
+		for (var i = 0; i < parsedData.length; i++){
+			var voornaam = parsedData[i].firstname
+			var achternaam = parsedData[i].lastname
+			if (voornaam.indexOf(input) >=0 || achternaam.indexOf(input)>=0){
+			match.push(voornaam + " " + achternaam);
+			count++
+				if(count == parsedData.length){
+					if (match.length == parsedData.length){
+						match = [];
+					}
+				}
+		}
+		else {
+			count++
+			if (count == parsedData.length) {
+				if (match.length == 0) {
+					match = ["Hahah niks hier sukkel"]
+				}
+			}
+		} 
+
+}
+
+		response.send({match:match});
+		console.log(match)
+
+		
+
+		
+
+
+});
 });
 
 app.post('/search', function( request, response){
 	fs.readFile('./users.json', function(err, data) {
-    var match = [];
+		var match = [];
 
-    if (err) {
-      console.log(err);
-    }
+		if (err) {
+			console.log(err);
+		}
 
-    var parsedData = JSON.parse(data);
+		var parsedData = JSON.parse(data);
 
-    console.log('parseddata')
-    console.log(parsedData)
-    
-    var input = request.body.userinput
-    for (var i = 0; i < parsedData.length; i++) {
-    	if (input === parsedData[i].firstname){
-    	match.push(parsedData[i]);
-    	}
-      if (input === parsedData[i].lastname){
-      match.push(parsedData[i]);
-      }
-  }
+		console.log('parseddata')
+		console.log(parsedData)
+		
+		var input = request.body.userinput
+		for (var i = 0; i < parsedData.length; i++) {
+			if (input === parsedData[i].firstname){
+			match.push(parsedData[i]);
+			}
+			if (input === parsedData[i].lastname){
+			match.push(parsedData[i]);
+			}
+	}
 
-    // match = JSON.stringify(match);
-   	if (match.length == 0){
-      console.log("no user found!")
-    } else {
-    console.log(match[0].firstname)
-    console.log(input)
-  }
+		// match = JSON.stringify(match);
+		if (match.length == 0){
+			console.log("no user found!")
+		} else {
+		console.log(match[0].firstname)
+		console.log(input)
+	}
 	 response.render('results', {match: match})
-  });
+	});
 });
 
 
 
 app.get('/submit-user', function (request, response) {
-  response.render("submit", {
-  });
+	response.render("submit", {
+	});
 });
 
 
 app.post('/submit-user', function (request, response) {
-  var newuser = request.body
-  var userList = fs.readFileSync('./users.json')
-  var users = JSON.parse (userList)
-    users.push (newuser)
-  var userJSON = JSON.stringify (users)
-    fs.writeFileSync ('./users.json', userJSON)
-      response.redirect ('/')
+	var newuser = request.body
+	var userList = fs.readFileSync('./users.json')
+	var users = JSON.parse (userList)
+		users.push (newuser)
+	var userJSON = JSON.stringify (users)
+		fs.writeFileSync ('./users.json', userJSON)
+			response.redirect ('/')
  });
 	
 
 var server = app.listen(3000, function() {
-  console.log('Example app listening on port: ' + server.address().port);
+	console.log('Example app listening on port: ' + server.address().port);
 });
